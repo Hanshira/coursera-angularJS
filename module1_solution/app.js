be3049b2981de25ca0fa2c7dac0505afd9e2de05
Hanna Mazzoni <hannamaz@gmail.com>
2018-05-16 22:28:50 +0200

    last essay

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -12,14 +12,14 @@
 
         $scope.checkList = function () {
             let lunchArray = $scope.lunchList.split(",");
-            let emplyElement = " ";
-            let index = 0;
-            while (index != -1) {
-                lunchArray.splice(lunchArray.indexOf(emplyElement, index), 1)
-                index = lunchArray.indexOf(emplyElement);
+            if (lunchArray.indexOf(" ") != -1) {
+                let index = 0;
+                while (index != -1) {
+                    lunchArray.splice(lunchArray.indexOf(" ", index), 1)
+                    index = lunchArray.indexOf(" ");
+                }
             }
             let lunchNumber = lunchArray.length;
-            console.log(lunchArray, lunchNumber)
             if (lunchNumber === 1 && lunchArray[0] === "") $scope.message = "Please enter data first"
             else if (lunchNumber <= 3) $scope.message = "Enjoy!";
             else $scope.message = "Too much!";
